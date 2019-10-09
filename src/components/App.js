97f38bc1fa00bf52865ec6d4b8ac70cf485b3de0
bvasilop <bvasilop@gmail.com>
2019-10-09 11:57:25 -0700

    implemented dark mode

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Content from './Content';
+import DarkModeToggle from './DarkModeToggle';
 import SearchBar from './SearchBar';
 import Scroll from './Scroll';
 import VideoDetail from './VideoDetail';
 import VideoList from './VideoList';
 import youtube from '../apis/youtube';
+
+import './styles.scss';
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -32,17 +36,28 @@ class App extends React.Component {
   render() {
     const { videos, selectedVideo } = this.state;
     return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="sixteen wide mobile sixteen wide tablet eleven wide computer column">
-              <VideoDetail video={selectedVideo} />
-            </div>
-            <div className="sixteen wide mobile sixteen wide tablet five wide computer column">
-              <Scroll>
-                <VideoList onVideoSelect={this.onVideoSelect} videos={videos} />
-              </Scroll>
+      <div>
+        <div className="navbar">
+          <DarkModeToggle />
+        </div>
+        <div className="content">
+          <Content />
+        </div>
+        <div className="ui container">
+          <SearchBar onFormSubmit={this.onTermSubmit} />
+          <div className="ui grid">
+            <div className="ui row">
+              <div className="sixteen wide mobile sixteen wide tablet eleven wide computer column">
+                <VideoDetail video={selectedVideo} />
+              </div>
+              <div className="sixteen wide mobile sixteen wide tablet five wide computer column">
+                <Scroll>
+                  <VideoList
+                    onVideoSelect={this.onVideoSelect}
+                    videos={videos}
+                  />
+                </Scroll>
+              </div>
             </div>
           </div>
         </div>
